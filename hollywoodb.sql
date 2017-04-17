bd21d84d9ae5e3edd4119a0e9c703501485bd9aa
Alex Duncanson <duncansa@mail.gvsu.edu>
2017-04-17 12:40:56 -0400

    Add files via upload
    Updated database with all integrity constraint tests written

--- hollywoodb.sql
+++ hollywoodb.sql
@@ -1,8 +1,4 @@
 --TODO:
--- BOXOFFICE inserts need to accomodate for costOfProduction
--- WORKS_ON  new table, we need data for
--- DISTRIBUTES new table, we need data for
---
 --
 
 SPOOL project.out
@@ -66,10 +62,6 @@ CREATE TABLE box_office (
 --
 CONSTRAINT boIC1 CHECK(openingWeekendRevenue >= 0)
 );
---
-CREATE TABLE roles (
-  personName		        varchar2(50) PRIMARY KEY,
-  personRole				    varchar2(50)
 --
 );
 --
@@ -146,18 +138,26 @@ INSERT INTO movie VALUES ('Singin in the Rain', 'MGM', '102','romance',4);
 INSERT INTO movie VALUES ('It Happened One Night', 'Sony Pictures Home Entertainment', '105','romance',3);
 INSERT INTO movie VALUES ('Casablanca', 'Warner Bros. Pictures', '102','romance',4);
 --
---testing(IC name)
+-- testing (mIC1)
+
+-- testing (mIC2)
+INSERT INTO movie VALUES ('Life', 'Warner Bros. Pictures', '117','romance',8);
 --
 -- person inserts
-INSERT INTO person VALUES('Will Smith','PW Management','10/15/16','43','m');
-INSERT INTO person VALUES('John Smith','PW Management','03/22/14','47','m');
-INSERT INTO person VALUES('Jason Momoa','JR Agencies','01/19/12','39','m');
-INSERT INTO person VALUES('Emma Watson','PW Management','07/25/09','28','f');
-INSERT INTO person VALUES('Abby Hall','JR Agencies','02/04/13','31','f');
-INSERT INTO person VALUES('Seth Rogen','JR Agencies','09/27/15','34','m');
-INSERT INTO person VALUES('Emily Jones','EL Management','09/04/16','42','f');
-INSERT INTO person VALUES('George Mann','EL Management','05/15/12','53','m');
--- testing (IC name)
+INSERT INTO person VALUES('Will Smith','PW Management','10/15/16',43,'m');
+INSERT INTO person VALUES('John Smith','PW Management','03/22/14',47,'m');
+INSERT INTO person VALUES('Jason Momoa','JR Agencies','01/19/12',39,'m');
+INSERT INTO person VALUES('Emma Watson','PW Management','07/25/09',28,'f');
+INSERT INTO person VALUES('Abby Hall','JR Agencies','02/04/13',31,'f');
+INSERT INTO person VALUES('Seth Rogen','JR Agencies','09/27/15',34,'m');
+INSERT INTO person VALUES('Emily Jones','EL Management','09/04/16',42,'f');
+INSERT INTO person VALUES('George Mann','EL Management','05/15/12',53,'m');
+--
+-- testing (pIC1)
+INSERT INTO person VALUES('Eli Joseph','EL Management','03/20/11',50,'n');
+-- testing (pIC2)
+INSERT INTO person VALUES('George Mann','EL Management','05/15/12',-40,'m');
+--
 -- production company inserts
 INSERT INTO production_company VALUES('Warner Bros. Pictures',1923);
 INSERT INTO production_company VALUES('RKO Radio Pictures',1928);
@@ -169,45 +169,60 @@ INSERT INTO production_company VALUES('Universal Pictures',1912);
 INSERT INTO production_company VALUES('United Artists',1919);
 INSERT INTO production_company VALUES('MGM',1924);
 INSERT INTO production_company VALUES('Sony Pictures Home Entertainment',1991);
--- testing (IC name)
+--
+-- testing (pcIC1)
+INSERT INTO movie VALUES('Warner Bros. Pictures',1700)
+--
 -- distributor inserts
-INSERT INTO distributor VALUES('Warner Bros. Pictures');
-INSERT INTO distributor VALUES('RKO Radio Pictures');
-INSERT INTO distributor VALUES('Rialto Pictures');
-INSERT INTO distributor VALUES('20th Century Fox');
-INSERT INTO distributor VALUES('Paramount Pictures');
-INSERT INTO distributor VALUES('Disney/Pixar');
-INSERT INTO distributor VALUES('Universal Pictures');
-INSERT INTO distributor VALUES('United Artists');
-INSERT INTO distributor VALUES('MGM');
-INSERT INTO distributor VALUES('Sony Pictures Home Entertainment');
+INSERT INTO distributor VALUES('Warner Bros. Pictures',’10/09/95’);
+INSERT INTO distributor VALUES('RKO Radio Pictures',’01/24/89’);
+INSERT INTO distributor VALUES('Rialto Pictures',’03/12/93’);
+INSERT INTO distributor VALUES('20th Century Fox',’07/05/82’);
+INSERT INTO distributor VALUES('Paramount Pictures',’11/20/74’);
+INSERT INTO distributor VALUES('Disney/Pixar',’12/17/77’);
+INSERT INTO distributor VALUES('Universal Pictures',’02/20/88’);
+INSERT INTO distributor VALUES('United Artists',’08/24/86’);
+INSERT INTO distributor VALUES('MGM',’09/03/97’);
+INSERT INTO distributor VALUES('Sony Pictures Home Entertainment',’10/24/03’);
+-- testing (IC name)
+-- distributes inserts
+INSERT INTO distributes VALUES(‘Warner Bros. Pictures’,’The Wizard of Oz’,’10/24/03’,’dvd’,’United States’);
+INSERT INTO distributes VALUES(‘RKO Radio Pictures’,’Citizen Kane’,’10/20/04’,’dvd’,’United States’);
+INSERT INTO distributes VALUES(‘Rialto Pictures’,’The Third Man’,’07/18/00’,’dvd’,’United States’);
+INSERT INTO distributes VALUES(‘20th Century Fox’,’MM Fury Road’,’08/27/06’,’vcr’,’United States’);
+INSERT INTO distributes VALUES(‘Paramount Pictures’,’All About Eve’,’04/12/02’,’vcr’,’United States’);
+INSERT INTO distributes VALUES(‘Disney/Pixar’,’The Cabinet of Dr. Caligari’,’10/20/99’,’dvd’,’United States’);
+INSERT INTO distributes VALUES(‘Universal Pictures’,’Inside Out’,’02/24/05’,’vcr’,’United States’);
 --testing (IC name)
 -- box office inserts
-INSERT INTO box_office VALUES(1000000,'The Wizard of Oz',1235986,156872,985634587);
-INSERT INTO box_office VALUES(1235986,'Citizen Kane',1286475,968725,547862354);
-INSERT INTO box_office VALUES(2694853,'The Third Man',17854963,123748,234658971);
-INSERT INTO box_office VALUES(258741,'MM : Fury Road',1235986,854721,127653892);
-INSERT INTO box_office VALUES(175982,'All About Eve',17854963,765843,234856975);
-INSERT INTO box_office VALUES(1236548,'The Cabinet of Dr.Caligari',1596734,657921,542376854);
-INSERT INTO box_office VALUES(257683,'Inside Out',17854963,435678,954723612);
-INSERT INTO box_office VALUES(2456852,'The Godfather',17854963,349685,124578965);
-INSERT INTO box_office VALUES(1596734,'Metropolis',1235986,754865,325412578);
-INSERT INTO box_office VALUES(29758632,'Get Out',17854963,346218,236521452);
-INSERT INTO box_office VALUES(1746859,'Modern Times',1286475,546231,458756325);
-INSERT INTO box_office VALUES(1286475,'E.T. The Extra-Terrestrial',17854963,742131,632541258);
-INSERT INTO box_office VALUES(2369854,'Singin in the Rain',1235986,355684,147896321);
-INSERT INTO box_office VALUES(17854963,'It Happened One Night',1596734,241322,258967413);
-INSERT INTO box_office VALUES(1798652,'Casablanca',1286475,235687,563254100);
--- testing (IC name)
--- roles inserts
-INSERT INTO roles VALUES('Will Smith','actor');
-INSERT INTO roles VALUES('John Smith','booking agent');
-INSERT INTO roles VALUES('Jason Momoa','actor');
-INSERT INTO roles VALUES('Emma Watson','actress');
-INSERT INTO roles VALUES('Abby Hall','booking agent');
-INSERT INTO roles VALUES('Seth Rogen','actor');
-INSERT INTO roles VALUES('Emily Jones','director');
-INSERT INTO roles VALUES('George Mann','producer');
+INSERT INTO box_office VALUES('The Wizard of Oz',4897362,1235986,156872,985634587);
+INSERT INTO box_office VALUES('Citizen Kane',1235986,1286475,968725,547862354);
+INSERT INTO box_office VALUES('The Third Man',84927489,17854963,123748,234658971);
+INSERT INTO box_office VALUES('MM : Fury Road',1726374,1235986,854721,127653892);
+INSERT INTO box_office VALUES('All About Eve',1236453,17854963,765843,234856975);
+INSERT INTO box_office VALUES('The Cabinet of Dr.Caligari',1829374,1596734,657921,542376854);
+INSERT INTO box_office VALUES('Inside Out',1726456,17854963,435678,954723612);
+INSERT INTO box_office VALUES('The Godfather',1829476,17854963,349685,124578965);
+INSERT INTO box_office VALUES('Metropolis',1728475,1235986,754865,325412578);
+INSERT INTO box_office VALUES('Get Out',1827465,17854963,346218,236521452);
+INSERT INTO box_office VALUES('Modern Times',1827475,1286475,546231,458756325);
+INSERT INTO box_office VALUES('E.T. The Extra-Terrestrial',1728472,17854963,742131,632541258);
+INSERT INTO box_office VALUES('Singin in the Rain',1827462,1235986,355684,147896321);
+INSERT INTO box_office VALUES('It Happened One Night',1829423,1596734,241322,258967413);
+INSERT INTO box_office VALUES('Casablanca',1829423,1286475,235687,563254100);
+--
+-- testing (boIC1)
+INSERT INTO box_office VALUES('Life',1829423,1286475,-235687,563254100);
+--
+-- works_on inserts
+INSERT INTO works_on VALUES(‘Will Smith’,’actor’);
+INSERT INTO works_on VALUES(‘John Smith’,’booking agent’);
+INSERT INTO works_on VALUES(‘Jason Momoa’,’actor’);
+INSERT INTO works_on VALUES(‘Emma Watson’,’actress’);
+INSERT INTO works_on VALUES(‘Abby Hall’,’booking agent’);
+INSERT INTO works_on VALUES(‘Seth Rogen’,’actor’);
+INSERT INTO works_on VALUES(‘Emily Jones’,’director’);
+INSERT INTO works_on VALUES(‘George Mann’,’producer’);
 -- testing (IC name)
 -- awards inserts
 INSERT INTO awards VALUES ('The Wizard of Oz','Film of The Year');
